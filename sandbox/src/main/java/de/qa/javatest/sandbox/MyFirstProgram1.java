a77c46629ac94c185dea82fe537a7b0b0463e3e1
Zarina Karymbaeva <zkarymbaeva@gmail.com>
2020-04-30 21:41:50 +0200

    Code format is changed.

--- sandbox/src/main/java/de/qa/javatest/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/de/qa/javatest/sandbox/MyFirstProgram1.java
@@ -2,8 +2,8 @@ package de.qa.javatest.sandbox;
 
 public class MyFirstProgram1 {
 
-public static void main(String[] args){
-	System.out.println("Hello, world!");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
 
 }
