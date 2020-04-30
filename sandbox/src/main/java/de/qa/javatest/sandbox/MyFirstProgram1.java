1910bf746e0a01142b10346cf694855789dc8d60
Zarina Karymbaeva <zkarymbaeva@gmail.com>
2020-04-30 23:21:56 +0200

    some functions are added

--- sandbox/src/main/java/de/qa/javatest/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/de/qa/javatest/sandbox/MyFirstProgram1.java
@@ -1,9 +1,22 @@
 package de.qa.javatest.sandbox;
 
+import java.util.function.DoubleToIntFunction;
+
 public class MyFirstProgram1 {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    double length = 5;
+    System.out.println("Плошадь со стороной "+length+" = " +area(length)+"!");
+    double a = 6;
+    double b = 6;
+    System.out.println("Плошадь со стороной "+a+" и "+b+"=" +area(a,b)+"!");
   }
 
+  public static double area(double length){
+    return length*length;
+  }
+
+  public static double area(double a, double b){
+    return a*b;
+  }
 }
